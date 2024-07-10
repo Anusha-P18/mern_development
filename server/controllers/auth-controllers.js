1d2ab793b3bca0808c8fa0131c5db9bd052312ad
Anusha S Puranikmath <anusha.puranikmath47@gmail.com>
2024-07-11 01:15:29 +0530

    Added user registration logic

--- server/controllers/auth-controllers.js
+++ server/controllers/auth-controllers.js
@@ -1,3 +1,4 @@
+const User = require("../models/user-model")
 // Home Logic
 
 const home = async(req, res) => {
@@ -12,9 +13,19 @@ const home = async(req, res) => {
 const register = async(req, res) => {
     try {
         console.log(req.body);
-        res.status(200).json({message: req.body});
+        const { username, email, phone, password} = req.body;
+
+        const userExist = await User.findOne({ email })
+
+        if( userExist ){
+            return res.status(400).json({ msg: "email already exixts"})
+        }
+
+        const userCreated = await User.create({ username, email, phone, password })
+
+        res.status(200).json({ message: userCreated})
     }catch (err) {
-        res.status(404).send({msg: "page not found"})
+        res.status(500).send({msg: "Internal server error"})
     }
 };
 
