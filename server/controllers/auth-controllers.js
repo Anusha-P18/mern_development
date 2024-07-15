286d285aa9077ab92d8a9f9f800de12450bec064
Anusha S Puranikmath <anusha.puranikmath47@gmail.com>
2024-07-15 21:43:28 +0530

    Added login functionality

--- server/controllers/auth-controllers.js
+++ server/controllers/auth-controllers.js
@@ -1,4 +1,5 @@
 const User = require("../models/user-model");
+const bcrypt = require("bcrypt");
 
 // Home Logic
 const home = async(req, res) => {
@@ -34,4 +35,38 @@ const register = async(req, res) => {
     }
 };
 
-module.exports = { home, register };
+// Login logic
+const login = async(req, res) => {
+    try {
+        const { email, password} = req.body;
+
+        const userExist = await User.findOne({ email });
+
+        if (!userExist) {
+            return res.status(400).json({
+                message: "Invalid credentials"
+            })
+        }
+
+        // compare password
+        const passwordExist = await bcrypt.compare(password, userExist.password);
+
+        if (passwordExist) {
+            res.status(200).json({ 
+                message: "Login successfull",
+                token: await userExist.generateToken(),
+                userId: userExist._id.toString(),
+            })
+        } else {
+            res.status(401).json({
+                message: "Invalid email or password"
+            })
+        }
+
+    }catch (err) {
+        console.log(err);
+    }
+};
+
+
+module.exports = { home, register, login };
