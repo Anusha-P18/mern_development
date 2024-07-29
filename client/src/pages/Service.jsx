4580176e34bfc50a2660ad795047218a5845869d
Anusha S Puranikmath <anusha.puranikmath47@gmail.com>
2024-07-29 22:16:39 +0530

    Fetching all services and displaying in Services page in form of cards.

--- client/src/pages/Service.jsx
+++ client/src/pages/Service.jsx
@@ -1,3 +1,37 @@
+import { useEffect } from "react"
+import { useAuth } from "../store/auth"
+
 export const Service = () => {
-    return <h1>This is Service page</h1>
+
+    const { services } = useAuth();
+    return (
+        <section className="section-services">
+            <div className="container">
+                <h1 className="main-heading">Services</h1>
+            </div>
+            <div className="container grid grid-three-cols">
+                {
+                    services.map((currElement, index) => {
+                        const { provider, price, description, service} = currElement;
+                        return(
+                            <div className="card" key={index}>
+                                <div className="card-img">
+                                    <img src="/images/design.png" alt="services image" width="500" />
+                                </div>
+
+                                <div className="card-details">
+                                    <div className="grid grid-two-cols">
+                                        <p>{provider}</p>
+                                        <p>{price}</p>
+                                    </div>
+                                    <h2>{service}</h2>
+                                    <p>{description}</p>
+                                </div>
+                            </div>
+                        );
+                    })
+                }
+            </div>
+        </section>
+    )
 }
--- client/src/store/auth.jsx
+++ client/src/store/auth.jsx
@@ -6,6 +6,7 @@ export const AuthProvider = ({children}) => {
 
     const [token, setToken] = useState(localStorage.getItem("token"));
     const [user, setUser] = useState("");
+    const [services, setServices] = useState("");
 
     const storeTokenInLS = (serverToken) => {
         return localStorage.setItem("token", serverToken);
@@ -40,11 +41,28 @@ export const AuthProvider = ({children}) => {
         }
     };
 
+    const getServices = async () => {
+        try{
+            const response = await fetch("http://localhost:5000/api/data/services", {
+                method: "GET"
+            });
+
+            if(response.ok) {
+                const data = await response.json();
+                setServices(data.msg);
+            }
+
+        } catch (err) {
+            alert(`Error in fetching services ${err}`);
+        }
+    }
+
     useEffect(() => {
+        getServices();
         userAuthentication();
     }, []);
 
-    return <AuthContext.Provider value={{ isLoggedIn, storeTokenInLS, LogoutUser, user }}>
+    return <AuthContext.Provider value={{ isLoggedIn, storeTokenInLS, LogoutUser, user, services }}>
         {children}
     </AuthContext.Provider>
 };
